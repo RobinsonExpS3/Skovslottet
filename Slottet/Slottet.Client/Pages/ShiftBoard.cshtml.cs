57877992a57a6ac684416d5f351b915c1d857380
Megan <meganebsen@gmail.com>
2026-03-26 13:42:33 +0100

    Created ShiftBoardPage

---
+++ Slottet/Slottet.Client/Pages/ShiftBoard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Slottet.Client.Pages
+{
+    public class ShiftBoardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
