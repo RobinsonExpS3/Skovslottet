4c37f9d7589f5287639c4d018bfa1b2555cc07c4
David Armstrong <proxie89@gmail.com>
2026-03-27 13:32:50 +0100

    added-api-controllers

---
+++ Slottet/Slottet.API/Controllers/ResidentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slottet.API.Controllers
+{
+    public class ResidentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Slottet/Slottet.API/Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slottet.API.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
