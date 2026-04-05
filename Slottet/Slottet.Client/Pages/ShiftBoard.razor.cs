aa0ec27d574725387d3ab05bb69a1c03eb2fa0c9
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-05 21:40:01 +0200

    Changed colors to make it not just WHITE on WHITE

--- Slottet/Slottet.Client/Pages/ShiftBoard.razor.cs
+++ Slottet/Slottet.Client/Pages/ShiftBoard.razor.cs
@@ -3,6 +3,10 @@ using Slottet.Shared;
 
 namespace Slottet.Client.Pages
 {
+    /// <summary>
+    /// Represents the main component for displaying and managing the shift board view in the application.
+    /// </summary>
+    /// 
     public partial class ShiftBoard //Midlertidig codebehind. Skal lige prøve det af. :D
     {
         protected ShiftBoardViewDto? Model { get; set; }
