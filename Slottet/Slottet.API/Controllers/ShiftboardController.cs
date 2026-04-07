f0e83b0bf161c319700cf9d6c4106de7dfccbfca
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-07 15:13:00 +0200

    started on controller, it doesnt work right now

--- Slottet/Slottet.API/Controllers/ShiftboardController.cs
+++ Slottet/Slottet.API/Controllers/ShiftboardController.cs
@@ -1,7 +1,14 @@
-﻿namespace Slottet.API.Controllers
-{
-    public class ShiftboardController
-    {
+﻿using Microsoft.AspNetCore.Mvc;
+using Slottet.Application.Interfaces;
+using Slottet.Domain.Entities;
 
+namespace Slottet.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShiftboardController : Controller
+    {
+        private readonly IBaseRepository<>
     }
 }
