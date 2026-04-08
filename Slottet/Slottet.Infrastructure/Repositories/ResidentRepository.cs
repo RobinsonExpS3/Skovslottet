5d2d802752fa01bb639394f945aba70794f3f861
David Armstrong <proxie89@gmail.com>
2026-04-08 13:09:06 +0200

    added-repo-parameters

--- Slottet/Slottet.Infrastructure/Repositories/ResidentRepository.cs
+++ Slottet/Slottet.Infrastructure/Repositories/ResidentRepository.cs
@@ -8,7 +8,7 @@ namespace Slottet.Infrastructure.Repositories
     {
         protected override string SqlSelectAll => "SELECT * FROM vm_Resident";
 
-        protected override string SqlSelectById => "usp_Insert_Resident";
+        protected override string SqlSelectById => "usp_SelectById_Resident";
 
         protected override string SqlInsert => "usp_Insert_Resident";
 
@@ -20,19 +20,34 @@ namespace Slottet.Infrastructure.Repositories
 
         protected override Resident Map(IDataRecord r) => new Resident
         {
-
+            ResidentID = r.GetGuid(r.GetOrdinal("ResidentID")),
+            ResidentName = r.GetString(r.GetOrdinal("ResidentName")),
+            IsActive = r.GetBoolean(r.GetOrdinal("IsActive")),
+            //GroceryDayID = r.GetGuid(r.GetOrdinal("GroceryDayID"))
+            
         };
 
+        
         protected override void BindInsert(SqlCommand cmd, Resident e)
         {
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@ResidentID", SqlDbType.UniqueIdentifier).Value = e.ResidentID;
+            cmd.Parameters.Add("@ResidentName", SqlDbType.NVarChar).Value = e.ResidentName;
+            cmd.Parameters.Add("@IsActive", SqlDbType.Bit).Value = e.IsActive;
 
         }
         protected override void BindUpdate(SqlCommand cmd, Resident e)
         {
+            cmd.CommandType = CommandType.StoredProcedure;
 
+            cmd.Parameters.Add("@ResidentID", SqlDbType.UniqueIdentifier).Value = e.ResidentID;
+            cmd.Parameters.Add("@ResidentName", SqlDbType.NVarChar).Value = e.ResidentName;
+            cmd.Parameters.Add("@IsActive", SqlDbType.Bit).Value = e.IsActive;
         }
         public async Task DeleteAsync(Guid id)
         {
 
+        }
     }
 }
--- Slottet/Slottet.Infrastructure/Repositories/SpecialResponsibilityRepository.cs
+++ Slottet/Slottet.Infrastructure/Repositories/SpecialResponsibilityRepository.cs
@@ -1,10 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
+using Microsoft.Data.SqlClient;
+using Slottet.Domain.Entities;
 
 namespace Slottet.Infrastructure.Repositories
 {
-    class SpecialResponsibilityRepository : BaseRepository <T>
+    class SpecialResponsibilityRepository : BaseRepository <SpecialResponsibility>
     {
+        protected override string SqlSelectAll => "SELECT * FROM vm_SpecialResponsibility";
+
+        protected override string SqlSelectById => "usp_SelectById_SpecialResponsibility";
+
+        protected override string SqlInsert => "usp_Insert_SpecialResponsibility";
+
+        protected override string SqlUpdate => "usp_Update_SpecialResponsibility";
+
+        protected override string SqlDeleteById => "usp_Delete";
+
+        protected override Guid GetKey(SpecialResponsibility e) => e.SpecialResponsibilityID;
+
+        protected override SpecialResponsibility Map(IDataRecord r) => new SpecialResponsibility
+        {
+            SpecialResponsibilityID = r.GetGuid(r.GetOrdinal("SpecialResponsibilityID")),
+            TaskName = r.GetString(r.GetOrdinal("TaskName"))
+
+        };
+
+        protected override void BindInsert(SqlCommand cmd, SpecialResponsibility e)
+        {
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@SpecialResponsibilityID", SqlDbType.UniqueIdentifier).Value = e.SpecialResponsibilityID;
+            cmd.Parameters.Add("@TaskName", SqlDbType.NVarChar, 255).Value = e.TaskName;
+
+        }
+        protected override void BindUpdate(SqlCommand cmd, SpecialResponsibility e)
+        {
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@SpecialResponsibilityID", SqlDbType.UniqueIdentifier).Value = e.SpecialResponsibilityID;
+            cmd.Parameters.Add("@TaskName", SqlDbType.NVarChar, 255).Value = e.TaskName;
+        }
+
+        public async Task DeleteAsync (Guid id)
+        {
+
+        }
     }
 }
