58e5a0dbae267c5134a2519c4065f60c84af8386
Patrick Kokholm <patrickkokholm@gmail.com>
2026-04-08 11:06:33 +0200

    removed GroceryDay string from Resident entity

--- Slottet/Slottet.Domain/Entities/Resident.cs
+++ Slottet/Slottet.Domain/Entities/Resident.cs
@@ -6,7 +6,6 @@ namespace Slottet.Domain.Entities {
     public class Resident {
         public Guid ResidentID { get; set; }
         public string ResidentName { get; set; }
-        public string GroceryDay { get; set; }
         public bool IsActive { get; set; }
 
         public GroceryDay GroceryDayID { get; set; }
