a357b5f3877a5f4f725c403c8d6226241956cb88
David Armstrong <proxie89@gmail.com>
2026-04-08 11:23:49 +0200

    added-repo

---
+++ Slottet/Slottet.Infrastructure/Repositories/ResidentRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using Slottet.Domain.Entities;
+
+namespace Slottet.Infrastructure.Repositories
+{
+    public sealed class ResidentRepository : BaseRepository<Resident>
+    {
+        protected override string SqlSelectAll => "SELECT * FROM vm_Resident";
+
+        protected override string SqlSelectById => "usp_Insert_Resident";
+
+        protected override string SqlInsert => "usp_Insert_Resident";
+
+        protected override string SqlUpdate => "usp_Update_Resident";
+
+        protected override string SqlDeleteById => "usp_Delete";
+
+        protected override Guid GetKey(Resident e)
+        {
+
+        }
+
+        protected override Resident Map(IDataRecord r) => new Resident
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void BindInsert(SqlCommand cmd, Resident e)
+        {
+
+        }
+        protected override void BindUpdate(SqlCommand cmd, Resident e)
+        {
+
+        }
+        protected override void BindDelete(SqlCommand cmd, Resident e)
+        {
+
+        }
+    }
+}
