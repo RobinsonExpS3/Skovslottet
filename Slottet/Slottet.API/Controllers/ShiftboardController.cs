e73215e20b87aa94cc27d6f448643e93f2e8c477
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-10 22:03:36 +0200

    Out commented

--- Slottet/Slottet.API/Controllers/ShiftboardController.cs
+++ Slottet/Slottet.API/Controllers/ShiftboardController.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Slottet.Application.Interfaces;
-using Slottet.Domain.Entities;
-
-namespace Slottet.API.Controllers
+﻿namespace Slottet.API.Controllers
 {
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ShiftboardController : Controller
-    {
-        private readonly IBaseRepository<>
-    }
+    //[ApiController]
+    //[Route("api/[controller]")]
+    //public class ShiftboardController : Controller
+    //{
+    //    private readonly IBaseRepository<>
+    //}
 }
