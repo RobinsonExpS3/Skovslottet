6b7daa92aa64ab67e247b1337d4379cf8493d397
Patrick Kokholm <patrickkokholm@gmail.com>
2026-04-13 21:16:44 +0200

    Implemented StaffResident.cs linking class

---
+++ Slottet/Slottet.Domain/Entities/StaffResident.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slottet.Domain.Entities {
+    public class StaffResident {
+        public Guid StaffID { get; set; }
+        public Guid ResidentID { get; set; }
+
+        public Staff Staff { get; set; }
+        public Resident Resident { get; set; }
+    }
+}
