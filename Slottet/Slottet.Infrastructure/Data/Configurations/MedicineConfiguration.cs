eda9af02a88fd59a73e9a5ed9e21a604d927be26
Patrick Kokholm <patrickkokholm@gmail.com>
2026-04-14 13:42:16 +0200

    Implemented MedicineConfiguration

---
+++ Slottet/Slottet.Infrastructure/Data/Configurations/MedicineConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Slottet.Domain.Entities;
+
+namespace Slottet.Infrastructure.Data.Configurations {
+    public class MedicineConfiguration : IEntityTypeConfiguration<Medicine> {
+        public void Configure(EntityTypeBuilder<Medicine> entity) {
+            entity.HasKey(m => new { m.MedicineID });
+
+            entity.Property(m => m.MedicineTime)
+                .IsRequired();
+
+            entity.Property(m => m.MedicineGivenTime)
+                .IsRequired();
+
+            entity.Property(m => m.MedicineRegisteredTime)
+                .IsRequired();
+
+            entity.HasOne(r => r.Resident)
+                .WithMany(m => m.Medicines)
+                .HasForeignKey(r => r.MedicineID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
