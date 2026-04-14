bd3028ab6ecc0832641a7e9ed1a3c94879ac3fba
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-14 12:52:25 +0200

    Added ResidentConfig, to model the entity, so that we can use EFCore

---
+++ Slottet/Slottet.Infrastructure/Data/Configurations/ResidentConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Slottet.Domain.Entities;
+
+namespace Slottet.Infrastructure.Data.Configurations
+{
+    public class ResidentConfiguration : IEntityTypeConfiguration<Resident>
+    {
+        public void Configure(EntityTypeBuilder<Resident> entity)
+        {
+            entity.HasKey(r => new { r.ResidentID });
+
+            entity.Property(r => r.ResidentName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(r => r.IsActive)
+                .IsRequired();
+
+            entity.HasOne(r => r.GroceryDay)
+                .WithMany(g => g.Residents)
+                .HasForeignKey(r => r.GroceryDayID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasMany(r => r.Medicines)
+                .WithOne(m => m.Resident)
+                .HasForeignKey(m => m.ResidentID)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
