0437cc7ae8afb9210375658da2449474f5db5fd0
Patrick Kokholm <patrickkokholm@gmail.com>
2026-04-14 13:02:17 +0200

    Implemented Configuration for Phone

---
+++ Slottet/Slottet.Infrastructure/Data/Configurations/PhoneConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Slottet.Domain.Entities;
+
+namespace Slottet.Infrastructure.Data.Configurations {
+    public class PhoneConfiguration : IEntityTypeConfiguration<Phone> {
+        public void Configure(EntityTypeBuilder<Phone> entity) {
+            entity.HasKey(r => new { r.PhoneID });
+
+            entity.Property(r => r.PhoneNumber)
+                .IsRequired();
+
+            entity.HasOne(r => r.Department)
+                .WithMany(g => g.Phones)
+                .HasForeignKey(r => r.DepartmentID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
