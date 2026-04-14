009dbbfc1283bfb26eef602f373a8cf59b2c8f24
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-14 13:24:57 +0200

    Added ShiftBoard ShiftBoard to StaffShift

--- Slottet/Slottet.Infrastructure/Data/Configurations/StaffShiftConfiguration.cs
+++ Slottet/Slottet.Infrastructure/Data/Configurations/StaffShiftConfiguration.cs
@@ -2,15 +2,19 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Slottet.Domain.Entities;
 
-namespace Slottet.Infrastructure.Data.Configurations {
-    public class StaffShiftConfiguration : IEntityTypeConfiguration<StaffShift> {
-        public void Configure(EntityTypeBuilder<StaffShift> entity) {
+namespace Slottet.Infrastructure.Data.Configurations
+{
+    public class StaffShiftConfiguration : IEntityTypeConfiguration<StaffShift>
+    {
+        public void Configure(EntityTypeBuilder<StaffShift> entity)
+        {
             entity.HasKey(ss => new { ss.ShiftBoardID, ss.StaffID });
 
             entity.HasMany(s => s.Staffs)
-                .WithMany(s => s.Staff)
+                .WithOne(s => s.Staff)
                 .HasForeignKey(s => s.Staff)
                 .OnDelete(DeleteBehavior.Restrict);
+
             entity.HasMany(sb => sb.ShiftBoards)
                 .WithMany(s => s.ShiftBoard)
                 .HasForeignKey(sb => sb.ShiftBoardID)
