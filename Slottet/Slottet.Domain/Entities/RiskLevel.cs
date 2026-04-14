db9674d8b90485b54133117cb617768870aab986
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-14 13:58:52 +0200

    Added Risklevel config

--- Slottet/Slottet.Domain/Entities/RiskLevel.cs
+++ Slottet/Slottet.Domain/Entities/RiskLevel.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Slottet.Domain.Entities {
-    public class RiskLevel {
+﻿namespace Slottet.Domain.Entities
+{
+    public class RiskLevel
+    {
         public Guid RiskLevelID { get; set; }
         public string RiskLevelName { get; set; }
+
+        public ICollection<ResidentStatus> ResidentStatus { get; set; }
     }
 }
