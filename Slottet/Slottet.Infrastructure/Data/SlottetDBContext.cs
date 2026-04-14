f65badf2248af9b918cb65df423dd2d73d4e05af
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-14 14:39:09 +0200

    Added ApplyConfigurationsFromAssembly in SlottetDBContext to add all the config files

--- Slottet/Slottet.Infrastructure/Data/SlottetDBContext.cs
+++ Slottet/Slottet.Infrastructure/Data/SlottetDBContext.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Slottet.Infrastructure.Data
 {
     public class SlottetDBContext : DbContext
     {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SlottetDBContext).Assembly);
+        }
     }
 }
