f3ff991f15ebae6fa2b2154495b15cf2c828e7b2
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-14 13:13:50 +0200

    Added ShiftBoardConfiguration

---
+++ Slottet/Slottet.Infrastructure/Data/Configurations/ShiftBoardConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Slottet.Domain.Entities;
+
+namespace Slottet.Infrastructure.Data.Configurations
+{
+    public class ShiftBoardConfiguration : IEntityTypeConfiguration<ShiftBoard>
+    {
+        public void Configure(EntityTypeBuilder<ShiftBoard> entity)
+        {
+            entity.HasKey(sb => new { sb.ShiftBoardID });
+            entity.Property(sb => sb.ShiftType)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(sb => sb.StartDate)
+                .IsRequired();
+
+            entity.Property(sb => sb.EndDate)
+                .IsRequired();
+
+            entity.HasMany(ss => ss.StaffShifts)
+                .WithOne(sb => sb.ShiftBoard)
+                .HasForeignKey(ss => ss.ShiftBoardID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasMany(sr => sr.SpecialResponsibilities)
+                .WithOne(sb => sb.ShiftBoard)
+                .HasForeignKey(sr => sr.ShiftBoardID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
