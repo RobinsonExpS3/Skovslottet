9f404d7bba0fdddc742cd669d9c3d3216707357b
Patrick Kokholm <patrickkokholm@gmail.com>
2026-04-14 20:53:55 +0200

    Implemented GetAll for ShiftBoard API

--- Slottet/Slottet.API/Controllers/ShiftboardController.cs
+++ Slottet/Slottet.API/Controllers/ShiftboardController.cs
@@ -4,11 +4,20 @@ using Slottet.Domain.Entities;
 
 namespace Slottet.API.Controllers
 {
-
     [ApiController]
     [Route("api/[controller]")]
     public class ShiftboardController : Controller
     {
         private readonly IBaseRepository<ShiftBoard> _repository;
+
+        public ShiftboardController(IBaseRepository<ShiftBoard> repository) {
+            _repository = repository;
+        }
+
+        [HttpGet("ShiftBoards")]
+        public async Task<ActionResult<IEnumerable<ShiftBoard>>> GetAll() {
+            var shiftBoard = await _repository.GetAllAsync();
+            return Ok(shiftBoard);
+        }
     }
 }
