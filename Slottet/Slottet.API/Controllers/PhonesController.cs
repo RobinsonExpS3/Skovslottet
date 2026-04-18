57f64e51328fc24929b91819a57f5ee371cb5249
Patrick Kokholm <patrickkokholm@gmail.com>
2026-04-18 22:46:44 +0200

    Used scaffolding to implement PhoneController API

---
+++ Slottet/Slottet.API/Controllers/PhonesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Slottet.Domain.Entities;
+using Slottet.Infrastructure.Data;
+
+namespace Slottet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhonesController : ControllerBase
+    {
+        private readonly SlottetDBContext _context;
+
+        public PhonesController(SlottetDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Phones
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Phone>>> GetPhones()
+        {
+            return await _context.Phones.ToListAsync();
+        }
+
+        // GET: api/Phones/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Phone>> GetPhone(Guid id)
+        {
+            var phone = await _context.Phones.FindAsync(id);
+
+            if (phone == null)
+            {
+                return NotFound();
+            }
+
+            return phone;
+        }
+
+        // PUT: api/Phones/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPhone(Guid id, Phone phone)
+        {
+            if (id != phone.PhoneID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(phone).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PhoneExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Phones
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Phone>> PostPhone(Phone phone)
+        {
+            _context.Phones.Add(phone);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPhone", new { id = phone.PhoneID }, phone);
+        }
+
+        // DELETE: api/Phones/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhone(Guid id)
+        {
+            var phone = await _context.Phones.FindAsync(id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+
+            _context.Phones.Remove(phone);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PhoneExists(Guid id)
+        {
+            return _context.Phones.Any(e => e.PhoneID == id);
+        }
+    }
+}
