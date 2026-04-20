ba9fd88c554f4904be1c7eac7e06f17f2225ecfa
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-20 15:01:24 +0200

    Added    public DateTime PNRegisteredTime { get; set; }  to PN class

--- Slottet/Slottet.Domain/Entities/PN.cs
+++ Slottet/Slottet.Domain/Entities/PN.cs
@@ -5,6 +5,7 @@
         public Guid PNID { get; set; }
         public DateTime PNGivenTime { get; set; }
         public string PNStatus { get; set; }
+        public DateTime PNRegisteredTime { get; set; }
 
         public Guid ResidentID { get; set; }
         public Resident Resident { get; set; }
