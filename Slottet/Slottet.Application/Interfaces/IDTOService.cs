26018eb8fab14c478a9fe3e82545961bbf2663e1
Megan <meganebsen@gmail.com>
2026-04-20 10:26:59 +0200

    IMedicineService

---
+++ Slottet/Slottet.Application/Interfaces/IDTOService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Slottet.Application.Interfaces
+{
+    public interface IDTOService : IController
+    {
+        async Task<ActionResult<IEnumerable<T>>> Getall();
+
+
+    }
+}
---
+++ Slottet/Slottet.Application/Interfaces/IMedicineService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Slottet.Shared;
+
+namespace Slottet.Application.Interfaces
+{
+    public interface IMedicineService
+    {
+        Task<IEnumerable<MedicineAPI_DTO>> GetAllAsync();
+        Task<MedicineAPI_DTO> GetByIdAsync(Guid id);
+        Task<MedicineAPI_DTO> CreateAsync(MedicineAPI_DTO dto);
+        Task UpdateAsync(Guid id, MedicineAPI_DTO dto);
+        Task DeleteAsync(Guid id);
+    }
+}
