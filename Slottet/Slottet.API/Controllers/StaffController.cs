5735969a9f6b73558e544026ef4241db5baa1b18
Patrick Kokholm <patrickkokholm@gmail.com>
2026-04-22 14:23:24 +0200

    Fixed an indentation issue

--- Slottet/Slottet.API/Controllers/StaffController.cs
+++ Slottet/Slottet.API/Controllers/StaffController.cs
@@ -1,97 +1,96 @@
-﻿
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
-    using Slottet.Application.Interfaces;
-    using Slottet.Domain.Entities;
-    using Slottet.Infrastructure.Data;
-    using Slottet.Shared;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Slottet.Application.Interfaces;
+using Slottet.Domain.Entities;
+using Slottet.Infrastructure.Data;
+using Slottet.Shared;
 
-    namespace Slottet.API.Controllers
+namespace Slottet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StaffController : Controller 
     {
-        [ApiController]
-        [Route("api/[controller]")]
-        public class StaffController : Controller 
+        private readonly IStaffDTOService _staffService;
+
+        public StaffController(IStaffDTOService staffService)
         {
-            private readonly IStaffDTOService _staffService;
+            _staffService = staffService;
+        }
 
-            public StaffController(IStaffDTOService staffService)
+        //Get: Staffs
+        [HttpGet("Staffs")]
+        public async Task<ActionResult<IEnumerable<StaffDTO>>> GetAllAsync()
+        {
+            var staffs = await _staffService.GetAllAsync();
+            
+            return Ok(staffs);
+        }
+
+        //Get: Staff by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StaffDTO>> GetByIdAsync(Guid id)
+        {
+            var staff = await _staffService.GetByIdAsync(id);
+
+            if (staff == null)
             {
-                _staffService = staffService;
+                return NotFound();
             }
 
-            //Get: Staffs
-            [HttpGet("Staffs")]
-            public async Task<ActionResult<IEnumerable<StaffDTO>>> GetAllAsync()
+            return Ok(staff);
+        }
+
+        //Post: Staff
+        [HttpPost]
+        public async Task<ActionResult<StaffDTO>> CreateAsync([FromBody] StaffDTO dto)
+        {
+            if (dto == null ||
+                string.IsNullOrWhiteSpace(dto.StaffName) ||
+                string.IsNullOrWhiteSpace(dto.Initials) ||
+                string.IsNullOrWhiteSpace(dto.Role))
             {
-                var staffs = await _staffService.GetAllAsync();
-                
-                return Ok(staffs);
+                return BadRequest();
             }
 
-            //Get: Staff by id
-            [HttpGet("{id}")]
-            public async Task<ActionResult<StaffDTO>> GetByIdAsync(Guid id)
+            var result = await _staffService.CreateAsync(dto);
+
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = dto.StaffID }, result);
+        }
+
+        //Put: Staff by id
+        [HttpPut("{id}")]
+        public async Task<ActionResult<StaffDTO>> UpdateAsync(Guid id, [FromBody] StaffDTO dto)
+        {
+            if (dto == null ||
+                string.IsNullOrWhiteSpace(dto.StaffName) ||
+                string.IsNullOrWhiteSpace(dto.Initials) ||
+                string.IsNullOrWhiteSpace(dto.Role))
             {
-                var staff = await _staffService.GetByIdAsync(id);
-
-                if (staff == null)
-                {
-                    return NotFound();
-                }
-
-                return Ok(staff);
+                return BadRequest();
             }
 
-            //Post: Staff
-            [HttpPost]
-            public async Task<ActionResult<StaffDTO>> CreateAsync([FromBody] StaffDTO dto)
-            {
-                if (dto == null ||
-                    string.IsNullOrWhiteSpace(dto.StaffName) ||
-                    string.IsNullOrWhiteSpace(dto.Initials) ||
-                    string.IsNullOrWhiteSpace(dto.Role))
-                {
-                    return BadRequest();
-                }
-
-                var result = await _staffService.CreateAsync(dto);
-
-                return CreatedAtAction(nameof(GetByIdAsync), new { id = dto.StaffID }, result);
+            var updated = await _staffService.UpdateAsync(id, dto);
+            
+            if(!updated) {
+                return NotFound();
             }
 
-            //Put: Staff by id
-            [HttpPut("{id}")]
-            public async Task<ActionResult<StaffDTO>> UpdateAsync(Guid id, [FromBody] StaffDTO dto)
+            return NoContent();
+        }
+
+        //Delete: Staff by id
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(Guid id)
+        {
+            var existingStaff = await _staffService.DeleteAsync(id);
+
+            if (!existingStaff)
             {
-                if (dto == null ||
-                    string.IsNullOrWhiteSpace(dto.StaffName) ||
-                    string.IsNullOrWhiteSpace(dto.Initials) ||
-                    string.IsNullOrWhiteSpace(dto.Role))
-                {
-                    return BadRequest();
-                }
-
-                var updated = await _staffService.UpdateAsync(id, dto);
-                
-                if(!updated) {
-                    return NotFound();
-                }
-
-                return NoContent();
+                return NotFound();
             }
 
-            //Delete: Staff by id
-            [HttpDelete("{id}")]
-            public async Task<ActionResult> DeleteAsync(Guid id)
-            {
-                var existingStaff = await _staffService.DeleteAsync(id);
-
-                if (!existingStaff)
-                {
-                    return NotFound();
-                }
-
-                return NoContent();
-            }
+            return NoContent();
         }
     }
+}
