71a00e3950687979797d1251cbb40bd881e24d92
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2026-04-27 12:12:17 +0200

    Updated Tabkles

---
+++ Slottet/Slottet.Infrastructure/Data/Configurations/StaffPNConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Slottet.Infrastructure.Data.Configurations
+{
+    public class StaffPNConfiguration : IEntityTypeConfiguration<StaffPN>
+    {
+        public void Configure(EntityTypeBuilder<StaffPN> Entity)
+        {
+
+        }
+    }
+}
