56cb496a41f9a9fbb6a11fccc8e53b225ca9a715
Megan <meganebsen@gmail.com>
2026-04-27 11:16:58 +0200

    NoteMedicin

---
+++ Slottet/Slottet.Application/Interfaces/INoteMedicineDTOService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slottet.Application.Interfaces
+{
+    public interface INoteMedicineDTOService
+    {
+        Task<IEnumerable<NoteMedicineDTO>> GetAllAsync();
+        Task<NoteMedicineDTO?> GetByIdAsync(Guid id);
+        Task<NoteMedicineDTO> CreateAsync(NoteMedicineDTO dto);
+        Task<bool> UpdateAsync(Guid id, NoteMedicineDTO dto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
