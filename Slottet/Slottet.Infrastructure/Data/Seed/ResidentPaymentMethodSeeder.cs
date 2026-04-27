a5c92ab3b6ab57725eb677915a57a41ec6eddde8
Megan <meganebsen@gmail.com>
2026-04-27 14:07:33 +0200

    ResidentPaymentMethodSeeder

--- Slottet/Slottet.Infrastructure/Data/Seed/ResidentPaymentMethodSeeder.cs
+++ Slottet/Slottet.Infrastructure/Data/Seed/ResidentPaymentMethodSeeder.cs
@@ -9,14 +9,35 @@ namespace Slottet.Infrastructure.Data.Seed
         {
             if (await context.ResidentPaymentMethods.AnyAsync()) return await context.ResidentPaymentMethods.ToListAsync();
 
+            var paymentMethods = new[] { "MobilePay", "P-kort", "Kontant", "Kort", "Udlæg"};
+
+            var paymentMethodyIDs = await context.PaymentMethods
+                .Where(p => paymentMethods.Contains(p.PaymentMethodName))
+                .ToDictionaryAsync(p => p.PaymentMethodName, p => p.PaymentMethodID);
+
+            Guid GetPaymentMethodID(string paymentMethod) => paymentMethodyIDs.TryGetValue(paymentMethod, out var id)
+                ? id
+                : throw new InvalidOperationException("Betalingsmetode ikke fundet");
+
+            var residents = new[] { "Carl Johan", "Trøffel", "Rød Fluesvamp", "Portobello", "Kantarel" };
+
+            var residentIDs = await context.Residents
+                .Where(r => residents.Contains(r.ResidentName))
+                .ToDictionaryAsync(r => r.ResidentName, r => r.ResidentID);
+
+            Guid GetResidentID(string resident) => residentIDs.TryGetValue(resident, out var id)
+                ? id
+                : throw new InvalidOperationException("Beboer ikke fundet");
+
+
             var residentPaymentMethods = new List<ResidentPaymentMethod>
             {
-                new ResidentPaymentMethod { ResidentID = Guid.NewGuid(), PaymentMethodID = Guid.NewGuid() },
-                new ResidentPaymentMethod { ResidentID = Guid.NewGuid(), PaymentMethodID = Guid.NewGuid() },
-                new ResidentPaymentMethod { ResidentID = Guid.NewGuid(), PaymentMethodID = Guid.NewGuid() },
-                new ResidentPaymentMethod { ResidentID = Guid.NewGuid(), PaymentMethodID = Guid.NewGuid() },
-                new ResidentPaymentMethod { ResidentID = Guid.NewGuid(), PaymentMethodID = Guid.NewGuid() },
-                new ResidentPaymentMethod { ResidentID = Guid.NewGuid(), PaymentMethodID = Guid.NewGuid() }
+                new ResidentPaymentMethod { ResidentID = GetResidentID("Carl Johan"), PaymentMethodID = GetPaymentMethodID("MobilePay") },
+                new ResidentPaymentMethod { ResidentID = GetResidentID("Trøffel"), PaymentMethodID = GetPaymentMethodID("MobilePay") },
+                new ResidentPaymentMethod { ResidentID = GetResidentID("Rød Fluesvamp"), PaymentMethodID = GetPaymentMethodID("MobilePay") },
+                new ResidentPaymentMethod { ResidentID = GetResidentID("Portobello"), PaymentMethodID = GetPaymentMethodID("Udlæg") },
+                new ResidentPaymentMethod { ResidentID = GetResidentID("Kantarel"), PaymentMethodID = GetPaymentMethodID("Kort") },
+                new ResidentPaymentMethod { ResidentID = GetResidentID("Kantarel"), PaymentMethodID = GetPaymentMethodID("Kontant") }
             };
 
             context.ResidentPaymentMethods.AddRange(residentPaymentMethods);
