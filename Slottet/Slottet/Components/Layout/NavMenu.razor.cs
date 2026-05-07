12b551955033fa128e9d1f55bebc92926cae3a03
Outlashed <Outlashed@Gmail.com>
2026-05-07 14:52:43 +0200

    Removed Admin-menu from the left navigation.

--- Slottet/Slottet/Components/Layout/NavMenu.razor.cs
+++ Slottet/Slottet/Components/Layout/NavMenu.razor.cs
@@ -1,9 +1,4 @@
 namespace Slottet.Components.Layout {
     public partial class NavMenu {
-        private bool isAdminOpen;
-
-        private void ToggleAdminMenu() {
-            isAdminOpen = !isAdminOpen;
-        }
     }
 }
